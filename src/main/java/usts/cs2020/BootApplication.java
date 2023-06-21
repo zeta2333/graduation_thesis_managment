5388c24d3ba3755f5c1117e2dd333a38bed8af62
Pycro <pycro6815@163.com>
2023-06-21 16:52:43 +0800

    6.21 system init

---
+++ src/main/java/usts/cs2020/BootApplication.java
@@ -0,0 +1,16 @@
+package usts.cs2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pycro
+ * @version 1.0
+ * 2023-06-21 3:56 PM
+ */
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
